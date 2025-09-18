54b7d3ee500c8dad33fceae664ec6578414a88f1
mean1010 <wlsalsrud05gmail.com>
2025-09-18 11:34:02 +0900

    20250918 수업최종

--- 20250918/variables.java
+++ 20250918/variables.java
@@ -22,6 +22,39 @@ public class variables {
         int wholeNumber = (int)pi;
         System.out.println(wholeNumber);
 
+        num1 = 7;
+        num2 = 3;
+
+        System.out.println("num1 + num2 = " + (num1 + num2));
+        System.out.println("num1 - num2 = " + (num1 - num2));
+        System.out.println("num1 * num2 = " + (num1 * num2));
+        System.out.println("num1 / num2 = " + (num1 / num2));
+        System.out.println("num1 % num2 = " + (num1 % num2));
+
+        num1 = 7;
+        num2 = 3;
+        //integr division
+        System.out.println("num1 / num2 = " + (num1 / num2));
+        //integr-point division
+        System.out.println("num1 / num2 = " +(((float)num1 / (float)num2)));
+        //Floating-point division
+        System.out.println("num1 / num2 = " +(float)(num1 / num2));
+        System.out.println("num1 / num2 = " +(num1 / (float)num2));
+
+        short num = 10;
+        num = (short)(num + 77L);       // 형 변환 안하면 컴파일 오류 발생
+        int rate = 3;
+        rate = (int)(rate * 3.5);       // 형 변환 안하면 컴파일 오류 발생
+        System.out.println(num);
+        System.out.println(rate);
+
+        num = 10;
+        num += 77L;                     // 형 변환 필요하지 않다.
+        rate = 3;
+        rate *= 3.5;                    // 형 변환 필요하지 않다.
+        System.out.println(num);
+        System.out.println(rate);
+
 
 
     }
